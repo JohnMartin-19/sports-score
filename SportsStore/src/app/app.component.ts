bfa12710b994c066e6459788e10084e75433175a
John <johnnkonge2020@gmail.com>
2024-02-02 12:01:47 +0300

    up and running

--- SportsStore/src/app/app.component.ts
+++ SportsStore/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
 @Component({
-  selector:"app",
-  template:`<div class="bg-success p-2 text-center text-white">
-    This is SportsStore
-  </div>`
+  selector: "app",
+standalone:true,
+  template: `<div class="bg-success p-2 text-center text-white">
+                This is SportsStore
+              </div>`
 })
-export class AppComponent{}
-
+export class AppComponent { }
--- SportsStore/src/main.ts
+++ SportsStore/src/main.ts
@@ -1,14 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import { AppComponent } from './app/app.component';
-import {enableProdMode} from '@angular/core';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-
-
-//if (environment.production){
- // enableProdMode();
-//}
-
-
+import { environment } from './environments/environment';
+if (environment.production) {
+  enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err))
+  .catch(err => console.error(err));

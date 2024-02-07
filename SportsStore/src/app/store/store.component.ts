6ee8b0e72582da4b112e05b0d0180705dcc70950
John <johnnkonge2020@gmail.com>
2024-02-07 13:22:01 +0300

    guarded routes

--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
 import { Cart } from "../model/cart.model";
+import { Router } from "@angular/router";
+
 @Component({
     selector: "store",
     templateUrl: "store.component.html"
@@ -11,7 +13,7 @@ export class StoreComponent {
     productsPerPage = 4;
     selectedPage = 1;
 
-    constructor(private repository: ProductRepository, private cart: Cart) { }
+    constructor(private repository: ProductRepository, private cart: Cart,private router:Router) { }
 
     get products(): Product[] {
         let pageIndex = (this.selectedPage - 1) * this.productsPerPage
@@ -44,5 +46,6 @@ export class StoreComponent {
     }
     addProductToCart(product:Product){
         this.cart.addLine(product);
+        this.router.navigateByUrl('/cart')
     }
 }

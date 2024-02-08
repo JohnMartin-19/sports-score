c7cc5f458537b7f0502839390d580671cc484749
John <johnnkonge2020@gmail.com>
2024-02-08 19:46:35 +0300

    updating orders

---
+++ SportsStore/src/app/model/order.model.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { Cart } from "./cart.model";
+
+@Injectable()
+export class Order{
+    public id?: number;
+    public name?: string;
+    public address?: string;
+    public city?:string;
+    public county?: string;
+    public country?:string;
+    public shipped?: boolean = false;
+
+    constructor(public cart:Cart){}
+
+    clear(){
+        this.id = undefined;
+        this.name= this.address=this.city=undefined;
+        this.county = this.country = undefined;
+        this.shipped = false;
+        this.cart.clear();
+    }
+}

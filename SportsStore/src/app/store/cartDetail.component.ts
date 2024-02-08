cf1cb8dcf2bdd06e1fd34d5c020757e9fcb418f4
John <johnnkonge2020@gmail.com>
2024-02-08 09:54:12 +0300

    cartDetail update

--- SportsStore/src/app/store/cartDetail.component.ts
+++ SportsStore/src/app/store/cartDetail.component.ts
@@ -1,7 +1,9 @@
 import { Component } from "@angular/core";
-
+import { Cart } from "../model/cart.model";
 
 @Component({
     template:'<div><h3 class="bg-info p1 text-white"> Cart Detail Component</h3></div>'
 })
-export class CartDetailComponent {}
+export class CartDetailComponent {
+    constructor(public cart:Cart) {}  // Injects the service into this component's scope so
+}

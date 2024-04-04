c5e7ae8b5c84fbf00c927ea4449629bae7b951ed
John <johnnkonge2020@gmail.com>
2024-04-04 18:29:29 +0300

    updating the admin

--- SportsStore/src/app/admin/material.module.ts
+++ SportsStore/src/app/admin/material.module.ts
@@ -1,4 +1,9 @@
 import { NgModule } from "@angular/core";
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatSidenavModule} from  '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon'
+import {MatDividerModule} from '@angular/material/divider'
+import {MatButtonModule} from '@angular/material/button'
 
 const features: any[] = [];
 
